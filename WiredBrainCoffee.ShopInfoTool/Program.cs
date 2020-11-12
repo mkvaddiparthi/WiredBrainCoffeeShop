21c997cd5c1a268b1024e3acdac966da25d92334
Murali Krishna Vaddiparthi <mkvaddiparthi@valuelabs.com>
2020-11-12 14:03:51 +0530

    Main console code implemented

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoffeeShopBusinessObjects;
+using System;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
@@ -7,6 +8,24 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Wire Brained Coffee Shop - For Asia region!");
-        }
+
+            Console.WriteLine("Type 'help' to list all available commands !");
+
+            var coffeeShopProvider = new CoffeeBusiness();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (string.Equals(line, "help", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> available coffee shop commands");
+                    foreach (var coffeshop in coffeeShopProvider.LoadCoffeeShops())
+                    {
+                        Console.WriteLine($"> " + coffeshop.Location);
+                    }
+                }
+            }
+        }        
     }
 }
