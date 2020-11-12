3af01ce1125c6ee7b0770310f9048570398b6a2f
Murali Krishna Vaddiparthi <mkvaddiparthi@valuelabs.com>
2020-11-12 14:03:18 +0530

    Business logic added

---
+++ CoffeeShopBusinessObjects/CoffeeBusiness.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CoffeeShopBusinessObjects
+{
+    public class CoffeeBusiness
+    {
+        public CoffeeBusiness()
+        {
+
+        }
+
+        public IEnumerable<CoffeeModel> LoadCoffeeShops()
+        {
+            yield return new CoffeeModel() { Location = "Manchester", StockInShop = 100 };
+            yield return new CoffeeModel() { Location = "London", StockInShop = 200 };
+            yield return new CoffeeModel() { Location = "Stockport", StockInShop = 300 };
+        }
+    }
+}
---
+++ CoffeeShopBusinessObjects/CoffeeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CoffeeShopBusinessObjects
+{
+    public class CoffeeModel
+    {
+        public string Location { get; set; }
+        public int StockInShop { get; set; }
+    }
+}
